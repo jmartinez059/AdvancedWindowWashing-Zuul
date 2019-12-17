510f717c49aa8bf204756569038ae7f3048b0172
Jason Martinez <jmartinez059@yahoo.com>
2019-12-16 20:10:17 -0600

    initializing git

---
+++ src/test/java/com/fifth/pillar/usercrudms/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifth.pillar.usercrudms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
